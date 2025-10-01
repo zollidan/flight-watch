9c1e4a9fca7ec74438a94b36af6c25f7dcaaff92
Stepan Fedosov <zollidan@yandex.ru>
2025-10-01 16:42:14 +0300

    Refactor fuel calculation logic and update user prompts to English

--- src/index.c
+++ src/index.c
@@ -2,27 +2,23 @@
 #include "fokker_f28_constants.h"
 
 // Примерная формула расчета топлива
-double calculate_fuel(double speed_knots, double distance_nm) {
-    double time = distance_nm / speed_knots; // время в часах
-    double fuel_needed = time * F28_MK1000_CRUISE_ECON_FLOW_KGH; // необходимое топливо в кг
+double calculate_fuel(const double distance_nm) {
+    const double time = distance_nm / F28_MK1000_CRUISE_ECON_SPEED_KIAS; // время в часах
+    const double fuel_needed = time * F28_MK1000_CRUISE_ECON_FLOW_KGH; // необходимое топливо в кг
 
     return fuel_needed;
 }
 
 int main() {
-    printf("Система расчета топлива для Fokker F28\n");
+    printf("Fuel Calculator for Fokker F28\n");
 
-    printf("Введите среднюю скорость полета (узлы): ");
-    double speed_knots;
-    scanf("%lf", &speed_knots);
-
-    printf("Введите дальность полета (морские мили): ");
+    printf("Enter flight distance (nautical miles): ");
     double distance_nm;
     scanf("%lf", &distance_nm);
 
-    double fuel_needed = calculate_fuel(speed_knots, distance_nm);
+    double fuel_needed = calculate_fuel(distance_nm);
 
-    printf("Необходимое топливо: %.2f кг\n", fuel_needed);
+    printf("Fuel needed: %.2f kg\n", fuel_needed);
 
     return 0;
 }
